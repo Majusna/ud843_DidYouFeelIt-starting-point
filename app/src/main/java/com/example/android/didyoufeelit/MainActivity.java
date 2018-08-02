383afc65942b5fc734f4041c3d78c5e1589cbe8a
piccolaMaja <todorovic.maja@hotmail.com>
2018-08-02 17:37:07 +0200

    moving the networking code off the main thread using an AsyncTask

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -19,7 +19,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-import android.os.AsyncTask;
+
+import java.net.URL;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -36,15 +37,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Create an {@link AsyncTask} to perform the HTTP request to the given URL
-        //on a background thread. When the result is received on the main UI thread,
-        //then update the UI.
+        // Create an {@link AsyncTask} to perform the HTTP request to the given URL
+        // on a background thread. When the result is received on the main UI thread,
+        // then update the UI.
         EarthquakeAsyncTask task = new EarthquakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
+        task.execute();
 
-
-        }
+    }
 
     /**
      * Update the UI with the given earthquake information.
@@ -60,11 +59,11 @@ public class MainActivity extends AppCompatActivity {
         magnitudeTextView.setText(earthquake.perceivedStrength);
     }
 
-    //{@link AsyncTask} to perform the network request on a background thread, and then
-    //update the UI with the first earthquake in the response.
-
-    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event>{
-
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread, and then
+     * update the UI with the first earthquake in the response.
+     */
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, Event> {
 
         /**
          * This method is invoked (or called) on a background thread, so we can perform
@@ -76,28 +75,30 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Event doInBackground(String... urls) {
 
-            // Don't perform the request if there are no URLs, or the first URL is null.
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
+            // Perform the HTTP request for earthquake data and process the response.
+            Event earthquake = Utils.fetchEarthquakeData(urls[0]);
+            return  earthquake;
 
-            Event result = Utils.fetchEarthquakeData(urls[0]);
-            return result;
             }
 
         /**
-         * This method is invoked on the main UI thread after the background work has been completed.
+         * This method is invoked on the main UI thread after the background work has been
+         * completed.
          *
          * It IS okay to modify the UI within this method. We take the {@link Event} object
          * (which was returned from the doInBackground() method) and update the views on the screen.
          */
+
         @Override
-        protected void onPostExecute(Event result){
-            // If there is no result, do nothing.
-            if (result == null) {
+        protected void onPostExecute(Event earthquake) {
+            if (earthquake == null) {
                 return;
             }
-            updateUi(result);
+
+            // Update the information displayed to the user.
+            updateUi(earthquake);
         }
+
+
     }
 }
